cb0734c391e8cfafa99d58baa62520e36062fc52
Chromss <nabilrahman330@gmail.com>
2024-09-18 08:35:43 +0700

    Tugas 3 Checkpoint

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-inventory/', show_add_inventory, name='show_add_inventory'),
     path('product/', show_product, name='show_product'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
 
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core import serializers
 from main.models import Product
 from main.forms import ProductForm
 
@@ -26,3 +28,19 @@ def show_add_inventory(request):
 def show_product(request):
     products = Product.objects.all()
     return render(request, 'product.html', {'products': products})
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
